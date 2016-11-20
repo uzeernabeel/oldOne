65a92f70bef42c413cdb68c662d7e2ed9d5332d7
Uzeer Mohammed <uzeernabeel@gmail.com>
2016-11-20 16:40:07 -0500

    some

--- core/src/com/newone/game/MyGdxGame.java
+++ core/src/com/newone/game/MyGdxGame.java
@@ -1,5 +1,6 @@
 package com.newone.game;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -8,44 +9,64 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.newone.game.WorldController;
+import com.newone.game.WorldRenderer;
 
 public class MyGdxGame extends ApplicationAdapter {
-	Texture img;
-	private OrthographicCamera camera;
-	private SpriteBatch batch;
-	private Texture texture;
-	private Sprite sprite;
-	
-	@Override
-	public void create () {
+    private static final String TAG = MyGdxGame.class.getName();
 
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
-		camera = new OrthographicCamera(1, h/w);
-		batch = new SpriteBatch();
-		texture = new Texture("badlogic.jpg");
-		texture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
-	}
+    private WorldController worldController;
+    private WorldRenderer worldRenderer;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		//batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		batch.draw(texture, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-		texture.dispose();
-	}
+    private boolean pause;
+
+
+    @Override
+    public void create() {
+        // Set Libgdx log level to DEBUG
+        Gdx.app.setLogLevel(Application.LOG_DEBUG);
+        // Initialize controller and renderer
+        worldController = new WorldController();
+        worldRenderer = new WorldRenderer(worldController);
+        // Game world is active on start
+        pause = false;
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        worldRenderer.resize(width, height);
+    }
+
+    @Override
+    public void render() {
+        //render the game only if not paused
+        // Do not update game world when paused.
+        if(!pause) {
+            // Update game world by the time that has passed since last rendered frame.
+            worldController.update(Gdx.graphics.getDeltaTime());
+        }
+        // Sets the clear screen color to: Cornflower Blue
+        Gdx.gl.glClearColor(0x64 / 255.0f, 0x95 / 255.0f, 0xed / 255.0f,
+                0xff / 255.0f);
+        // Clears the screen
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+            // Render game world to screen
+            worldRenderer.render();
+
+    }
+
+    @Override
+    public void pause() {
+        pause = true;
+    }
+
+    @Override
+    public void resume() {
+        pause = false;
+    }
+
+    @Override
+    public void dispose() {
+        worldRenderer.dispose();
+    }
 }
---
+++ core/src/com/newone/game/WorldController.java
@@ -0,0 +1,117 @@
+package com.newone.game;
+
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputAdapter;
+
+public class WorldController extends InputAdapter{
+    public Sprite[] testSprites;
+    public int selectedSprite;
+
+    private static final String TAG = WorldController.class.getName();
+
+    public WorldController(){
+        init();
+    }
+
+    public void init(){
+        Gdx.input.setInputProcessor(this);
+        initTestObjects();
+    }
+
+    @Override
+    public boolean keyUp(int keycode){
+        // Reset game world
+        if (keycode == Keys.R) {
+            init();
+            Gdx.app.debug(TAG, "Game world resetted");
+        }
+        // Select next sprite
+        else if (keycode == Keys.SPACE) {
+            selectedSprite = (selectedSprite + 1) % testSprites.length;
+            Gdx.app.debug(TAG, "Sprite #" + selectedSprite + " selected");
+        }
+        return false;
+    }
+
+    private void initTestObjects() {
+        // Create new array for 5 sprites
+        testSprites = new Sprite[5];
+        // Create empty POT-sized Pixmap with 8 bit RGBA pixel data
+        int width = 32;
+        int height = 32;
+        Pixmap pixmap = createProceduralPixmap(width, height);
+        // Create a new texture from pixmap data
+        Texture texture = new Texture(pixmap);
+        // Create new sprites using the just created texture
+        for (int i = 0; i < testSprites.length; i++) {
+            Sprite spr = new Sprite(texture);
+        // Define sprite size to be 1m x 1m in game world
+            spr.setSize(1, 1);
+        // Set origin to sprite's center
+            spr.setOrigin(spr.getWidth() / 2.0f, spr.getHeight() / 2.0f);
+        // Calculate random position for sprite
+            float randomX = MathUtils.random(-2.0f, 2.0f);
+            float randomY = MathUtils.random(-2.0f, 2.0f);
+            spr.setPosition(randomX, randomY);
+        // Put new sprite into array
+            testSprites[i] = spr;
+        }
+        // Set first sprite as selected one
+        selectedSprite = 0;
+    }
+
+    private Pixmap createProceduralPixmap (int width, int height) {
+        Pixmap pixmap = new Pixmap(width, height, Format.RGBA8888);
+        // Fill square with red color at 50% opacity
+        pixmap.setColor(1, 0, 0, 0.5f);
+        pixmap.fill();
+        // Draw a yellow-colored X shape on square
+        pixmap.setColor(1, 1, 0, 1);
+        pixmap.drawLine(0, 0, width, height);
+        pixmap.drawLine(width, 0, 0, height);
+        // Draw a cyan-colored border around square
+        pixmap.setColor(0, 1, 1, 1);
+        pixmap.drawRectangle(0, 0, width, height);
+        return pixmap;
+    }
+
+
+    public void update(float deltaTime){
+        handleDebugInput(deltaTime);
+        updateTestObjects(deltaTime);
+    }
+
+    private void handleDebugInput(float deltaTime) {
+        if (Gdx.app.getType() != Application.ApplicationType.Desktop) return;
+
+        // Selected Sprite Controls
+        float sprMoveSpeed = 5 * deltaTime;
+        if (Gdx.input.isKeyPressed(Keys.LEFT)) moveSelectedSprite(-sprMoveSpeed, 0);
+        if (Gdx.input.isKeyPressed(Keys.RIGHT)) moveSelectedSprite(sprMoveSpeed, 0);
+        if (Gdx.input.isKeyPressed(Keys.UP)) moveSelectedSprite(0, sprMoveSpeed);
+        if (Gdx.input.isKeyPressed(Keys.DOWN)) moveSelectedSprite(0, -sprMoveSpeed);
+
+    }
+
+    private void moveSelectedSprite (float x, float y) {
+        testSprites[selectedSprite].translate(x, y);
+    }
+
+    private void updateTestObjects(float deltaTime) {
+        // Get current rotation from selected sprite
+        float rotation = testSprites[selectedSprite].getRotation();
+        // Rotate sprite by 90 degrees per second
+        rotation += 90 * deltaTime;
+        // Wrap around at 360 degrees
+        rotation %= 360;
+        // Set new rotation value to selected sprite
+        testSprites[selectedSprite].setRotation(rotation);
+    }
+}
